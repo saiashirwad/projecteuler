7c21d836ba3d8feaae8cfec3ea5a0e4f69be4f94
Sai Ashirwad <saiashirwadr@gmail.com>
2021-04-13 11:09:18 +0530

    eh

--- 2.hs
+++ 2.hs
@@ -1,3 +1,2 @@
-fib = 0 : 1 : (zipWith (+) fib (tail fib))
-
-main = do putStrLn $ show $ sum $ filter even $ takeWhile (<4000000) fib
+main = do putStrLn $ show $ sum $ filter even $ takeWhile (<4000000) fib where
+        fib = 0 : 1 : (zipWith (+) fib (tail fib))
