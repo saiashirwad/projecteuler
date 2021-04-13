472dd034b5e27c9fc4c19dee5d817d08354724e0
Sai Ashirwad <saiashirwadr@gmail.com>
2021-04-13 12:07:33 +0530

    eh

--- 5.hs
+++ 5.hs
@@ -2,5 +2,4 @@ lcm' a b = (a * b) `div` gcd' a b where
         gcd' x 0 = x
         gcd' x y = gcd' y (x `mod` y)
 
-
-main = do putStrLn $ show $ foldr1 lcm' [1..20]
+main = do putStrLn $ show $ foldl1 lcm' [1..20]
